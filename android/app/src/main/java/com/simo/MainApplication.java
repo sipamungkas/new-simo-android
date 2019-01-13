eaa6dedb4f1e0dbb4b60a2e59372d9552e31e1b4
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2019-01-13 15:35:24 +0700

    Fungsi Login Firebase

--- android/app/src/main/java/com/simo/MainApplication.java
+++ android/app/src/main/java/com/simo/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,7 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
-            new RNFirebasePackage()
+            new RNFirebasePackage(),
+            new RNFirebaseAuthPackage()
       );
     }
 
--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
 import {StyleSheet, View, Text, ActivityIndicator, TextInput } from 'react-native';
-import {Button, Snackbar} from 'react-native-paper';
-import style from "./styles"
+import {Button, Snackbar, ToastAndroid} from 'react-native-paper';
+import style from "./styles";
+import firebase from "react-native-firebase";
+
 
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: null,
-      password: null,
+      data:null,
+      email: "",
+      password: "",
       loading: false,
+      errorMessage: null,
       btn : "Masuk",
       snack: false,
+      textSnack:null,
     };
+  }
+  componentWillMount(){
+    console.warn(firebase.auth().currentUser)
   }
   loginHandler = () => {
     this.setState({loading:true, btn: "Loading"});
-    setTimeout(() => {
-      this.setState({loading:false, btn:"Masuk", snack:true})
-    }, 2000);
+    if (this.state.email != "" && this.state.password != "") {
+      setTimeout(() => {
+        firebase.auth().signInWithEmailAndPassword(this.state.email,this.state.password)
+        .then(()=>this.setState({loading:false, btn:"Masuk", snack:true, textSnack:"Anda Berhasil Masuk", loading:false}))
+        .then(()=>setTimeout(this.props.navigation.navigate("Dashboard"),1000))
+        .catch((error)=>this.setState({errorMessage:error.code, btn:"Masuk",snack:true, textSnack:error.code, loading:false }))
+        
+      }, 2000);  
+    } else {
+      this.setState({textSnack:"Username atau Password Tidak Boleh Kosong", loading:false,snack:true, btn:"Masuk"})
+    }
+    
+  newLoginHandler = () => {
+    firebase.auth().signInWithEmailAndPassword(this.state.email,this.state.password).then((tes)=>console.warn(tes))
+    
+  }
     
     
   }
@@ -31,18 +52,19 @@ export default class Login extends Component {
       
         <Text style={heading}> Login ke Simo  </Text>
         
-        
+        <Text>{this.state.email && this.state.email}</Text>
+        <Text>{this.state.password && this.state.password}</Text>
         <TextInput underlineColorAndroid="gray" placeholder="Email" onChangeText={ (text) => this.setState({email:text})}/>
         
         <TextInput secureTextEntry underlineColorAndroid="gray" placeholder='password' onChangeText={ (text) => this.setState({password:text})} />
-        <Button color="blue" mode="contained" loading={this.state.loading} disabled={this.state.loading} onPress={this.loginHandler} >{this.state.btn}</Button>
+        <Button color="#900" mode="contained" loading={this.state.loading} disabled={this.state.loading} onPress={this.loginHandler} >{this.state.btn}</Button>
         <Text style={{textAlign:'center', padding:10}}>Atau</Text>
-        <Button color="blue" mode="contained" disabled={this.state.loading} onPress={() => console.warn('Daftar')} >Daftar</Button>
+        <Button color="#900" mode="contained" disabled={this.state.loading} onPress={() => console.warn('Daftar')} >Daftar</Button>
         <Snackbar
           visible={this.state.snack}
           onDismiss={() => this.setState({ snack: false })}
         >
-          Anda Berhasil Masuk
+          {this.state.textSnack}
         </Snackbar>
       
         
