9231f3f1f6a4723be9d853303f5fc2e8566f811b
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2019-01-25 00:31:45 +0700

    Peningkatan performa FlatList + Pagination Firebase

--- app/components/Aktifitas/index.js
+++ app/components/Aktifitas/index.js
@@ -11,22 +11,62 @@ export default class Aktifitas extends Component {
     this.state = {
       data:[],
       loading: true,
-      today:new Date()
+      today:new Date(),
+      key: "",
+      keyReference:null,
+      endPoint: 10
     };
   }
 
-  componentDidMount(){
-    let newData = [];
-    timezone = new Date().getTimezoneOffset();
+  firebaseFetch = () =>{
+    let oldData = this.state.data;
+    let referenceToOldestKey = this.state.keyReference;
+    if (referenceToOldestKey==null) {
+      const ref = firebase.database().ref("data/");
+      ref.child("pir").orderByKey().limitToLast(15)
+      .once("value").then((snapshot)=>{
+        let arrayOfKeys = Object.keys(snapshot.val())
+         .sort()
+         .reverse();
+      let results = arrayOfKeys
+         .map((key) => snapshot.val()[key]);
+      referenceToOldestKey = arrayOfKeys[arrayOfKeys.length-1];
+      this.setState({data:results,keyReference:referenceToOldestKey,loading:false})
+      }).catch((error)=>alert(error.code));  
+    }else{
+      const ref = firebase.database().ref("data/");
+      ref.child("pir").orderByKey().endAt(referenceToOldestKey)
+      .limitToLast(15)
+      .once("value").then((snapshot)=>{
+        let arrayOfKeys = Object.keys(snapshot.val())
+         .sort()
+         .reverse()
+         .slice(1);
+      let results = arrayOfKeys
+         .map((key) => snapshot.val()[key]);
+      
+      referenceToOldestKey = arrayOfKeys[arrayOfKeys.length-1];
+      this.setState({data:this.state.data.concat(results),keyReference:referenceToOldestKey,loading:false})
+      }).catch((error)=>alert("Terjadi Kesalahan"));  
+    }
+    
+  }
+  firebaseFetch2 = () =>{
+    let newData2 = this.state.data;
     const ref = firebase.database().ref("data/");
-    ref.child("pir").orderByChild("time").startAt("2019-01-19 00:00:00").endAt("2019-01-19 23:59:59").on("value", (snapshot)=>snapshot.forEach((doc)=>
+    ref.child("pir").orderByKey().limitToLast(6).endAt(this.state.key)
+    .on("value", (snapshot)=>snapshot.forEach((doc)=>
     {
-      newData.unshift(doc.val());
-      this.setState({data:newData, loading:false});
+      newData2.unshift(doc.val());
+      this.setState({data:newData2, loading:false});
+      key = doc.key;
+      console.warn(newData2)
     }));
-    
-    
-    
+  }
+
+  
+  componentDidMount(){  
+    this.firebaseFetch()
   }
   render() {
     const {viewContainerMargin, isiCardContent} = style
@@ -39,12 +79,14 @@ export default class Aktifitas extends Component {
     }
     return(
       <View style={viewContainerMargin}>
-      <ScrollView
-      contentContainerStyle={{flexDirection:"row",flexGrow:1}}
-      >
+      
+      
       <FlatList
-          scrollEnabled={false}
+          
           data={this.state.data}
+          onEndReachedThreshold={0.5}
+          onEndReached={this.firebaseFetch}
+          
           renderItem={({item}) => 
           <Card>
             <Card.Content style={{flex:1}}>
@@ -56,8 +98,8 @@ export default class Aktifitas extends Component {
           }
           keyExtractor={(item,index)=>index.toString()}
         />
-        </ScrollView>
-        
+      
+        {console.warn(this.state.data)}
         </View>
     );
     
